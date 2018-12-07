0b554f52c701dfcd0c2464af48dd2368e46b6a40
Dusan Randjelovic <dusanrandjelovic92@gmail.com>
2018-12-07 19:53:46 +0100

    Add files via upload

---
+++ zadatak.js
@@ -0,0 +1,22 @@
+novniz="";
+brojac=0;
+function odredi(){
+
+  niz=document.getElementById("br1").value;
+  i=niz.length-1;
+  
+  while (i>=0){
+    novniz+=niz[i]; //niz guramo u nov niz
+    brojac++;
+    if(brojac%3==0 && brojac<niz.length){
+      novniz+=" ,";
+    }
+    i--;
+  }
+
+  rezultat=" ";
+  for (i=novniz.length-1;i>=0;i--){
+    document.getElementById("br2").value+=novniz[i];
+  }
+
+}
